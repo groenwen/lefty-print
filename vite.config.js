ec4fdc96697ba7f828e74805999ed534001904e8
yawen <S2004001@samebest.com.tw>
2022-10-21 15:17:32 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/print-vite/',
+  base: '/lefty-print/',
   plugins: [vue()],
   resolve: {
     alias: {
