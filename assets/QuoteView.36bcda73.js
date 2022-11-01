34abc6ee253f376ca908353a845d9c355207a364
Wen <yuwen7005@gmail.com>
2022-11-02 00:43:25 +0800

    update

--- assets/QuoteView.36bcda73.js
+++
@@ -1,1 +0,0 @@
-import{H as t}from"./HeadTitle.aa06f509.js";import{_ as o,r as a,o as r,a as n,d as s}from"./index.887741cb.js";const c={components:{HeadTitle:t}};function p(d,i,l,_,m,f){const e=a("HeadTitle");return r(),n("div",null,[s(e,{dialog:"\u5831\u50F9","primary-text":"\u7279\u6B8A\u5C3A\u5BF8","dark-text":"\u5831\u50F9"})])}const H=o(c,[["render",p]]);export{H as default};
---
+++ assets/people01.1d41a424.js
@@ -0,0 +1,1 @@
+const s="/lefty-print/assets/people01.285baf15.svg";export{s as _};
